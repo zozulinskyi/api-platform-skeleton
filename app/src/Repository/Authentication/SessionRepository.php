432e94594c3adf919a98bf2f20b5e15737364f4b
Ihor Zozulinskyi <igor.zozulinskyi@gmail.com>
2025-02-16 18:51:42 +0200

    * add user extension to session entity;

--- app/src/Repository/Authentication/SessionRepository.php
+++ app/src/Repository/Authentication/SessionRepository.php
@@ -7,7 +7,6 @@ use App\Entity\Authentication\Session;
 use App\Entity\Authentication\User;
 use Carbon\Carbon;
 use Doctrine\Bundle\DoctrineBundle\Repository\ServiceEntityRepository;
-use Doctrine\ORM\Tools\Pagination\Paginator;
 use Doctrine\Persistence\ManagerRegistry;
 use Symfony\Component\HttpFoundation\Request;
 
@@ -54,19 +53,5 @@ final class SessionRepository extends ServiceEntityRepository
     public function exists(string $sessionId): bool
     {
         return (bool)$this->count(['id' => $sessionId]);
-    }
-
-    /**
-     * @return Paginator<Session>
-     */
-    public function getUserSessions(User $user, int $page = 1, int $limit = 10): Paginator
-    {
-        $query = $this->createQueryBuilder(alias: 's')
-            ->where(predicates: 's.user = :user')
-            ->setParameter(key: 'user', value: $user)
-            ->setMaxResults(maxResults: $limit)
-            ->setFirstResult(firstResult: ($page - 1) * $limit);
-
-        return new Paginator(query: $query, fetchJoinCollection: false);
     }
 }
